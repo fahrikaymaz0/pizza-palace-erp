c48aa1ef0b11651c16f3cfea82172af88e4958a2
fahrikaymaz0 <fahrikaymaz0@gmail.com>
2025-08-25 11:05:06 +0300

    fix: disable Next image optimizer for hero bg; robust default DATABASE_URL; CORS on chat APIs

--- pages/index.js
+++ pages/index.js
@@ -241,6 +241,7 @@ export default function RoyalPizzaKingdom() {
             fill
             priority
             sizes="100vw"
+            unoptimized
             style={{ objectFit: 'cover', filter: 'blur(6px) opacity(0.28)' }}
             onError={() => setBgSrc('/arkaplanpizza.png')}
           />
